6554f3bf4b69313b18a0d87833ca785a9b124cc1
me.sanjeevks@gmail.com <shantanubisaria@Affirmity-Mac.local>
2024-02-04 17:20:02 +0530

    starter code setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ fitfusion/android/app/src/main/kotlin/com/fitfusion/fitfusion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fitfusion.fitfusion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
